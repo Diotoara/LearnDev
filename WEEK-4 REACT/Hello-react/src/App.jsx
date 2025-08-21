6beffd59eb353ef588c58e21ff9c373c1254c255
Diotoara <aapka.5ryan@gmail.com>
2025-08-21 12:07:56 +0530

    learning react

--- WEEK-4 REACT/Hello-react/src/App.jsx
+++ WEEK-4 REACT/Hello-react/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function addf(){
+    setCount( count + 1 )
+  }
   return (
     <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={addf} > Counter : {count}</button>
+        <button onClick={addf} > Counter2 : {count+10}</button>
     </div>
   )
 }
