6e30e3740e603df0fd11b3bc549828262375a469
Diotoara <aapka.5ryan@gmail.com>
2025-08-27 13:09:45 +0530

    chnges

--- WEEK-4 REACT/Hello-react/src/App.jsx
+++ WEEK-4 REACT/Hello-react/src/App.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+    title : "wake at 6am",
+    description : "freshen up and active till 6:30",
+    done : true},
+    {title : "DSA",
+    description : "DSA from 6:30 to 9 AM",
+    done : false
+  }])
 
-  function addf(){
-    setCount( count + 1 )
-  }
+
   return (
     <div>
-        <button onClick={addf} > Counter : {count}</button>
-        <button onClick={addf} > Counter2 : {count+10}</button>
+      <EachTodo title = {todos[0].title} description = {todos[0].description}></EachTodo>
+      <br />
+      <EachTodo title = {todos[1].title} description = {todos[1].description}></EachTodo>
     </div>
-  )
+  ) 
 }
+  function EachTodo(prop){
+    return (
+      <div>
+        <div>{prop.title}</div>
+        <div>{prop.description}</div>
+      </div>
+    )
+  }
 
 export default App
