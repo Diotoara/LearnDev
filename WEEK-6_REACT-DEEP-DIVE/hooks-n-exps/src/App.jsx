ca9d08cbe08e92d5e09977628fb1e42220f3a55a
Diotoara <aapka.5ryan@gmail.com>
2025-08-29 01:59:22 +0530

    understanding_react-1

---
+++ WEEK-6_REACT-DEEP-DIVE/hooks-n-exps/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Headder( {collor,txt}) {
+  return(
+    <>
+      <h1 style={{
+        color : collor,
+        marginLeft : 20,
+        }}>{txt}</h1>
+    </>
+  )
+}
+
+
+function App() {
+
+  return (
+    <>
+      <Headder collor="red" txt="hello world" />
+      <Headder collor="blue" txt="this is aryan this side" />
+      <Headder collor="white" txt="aight, listen "/>
+    </>
+  )
+}
+
+export default App
+
