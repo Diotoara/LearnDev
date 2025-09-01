8828a3e9488271b67a296b28c42780382c54c98b
Diotoara <aapka.5ryan@gmail.com>
2025-09-01 12:19:22 +0530

    todo

--- WEEK-6_REACT-DEEP-DIVE/hooks-n-exps/src/App.jsx
+++ WEEK-6_REACT-DEEP-DIVE/hooks-n-exps/src/App.jsx
@@ -1,29 +1,40 @@
 import { useState } from 'react'
+import React from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-// function Headder( {txt}) {
-//   return(
-//     <>
-//       <h1>{txt}</h1>
-//     </>
-//   )
-// }
-
+function Todo({title, desc}){
+  return(
+    <>
+      <h1>{title}</h1>
+      {desc}
+    </>
+  )
+}
 
 function App() {
-  const [txt, setTxt] = useState("this will tell you today's weather")
 
-  function newHead(){
-    setTxt("today's weather is " + Math.random()*10 +" ");
-  }
+  const [todos, useTodos] = useState([{
+    id: 1,
+    title: "go to gym",
+    desc: "from 8 to 10 am"
+  }, {
+    id: 2,
+    title : "freshen up",
+    desc : "from 6 to 7 am",
+  }, {
+    id:3,
+    title:"have breakfast",
+    desc : "from 7 to 8 am"
+  }])
 
   return (
-    <>
-      <button  onClick={newHead}>Click to change top</button>
-      <br />
-      {txt}
+    <>    
+      {/* {todos.map(todo => <Todo title={todo.title} desc={todo.desc} />)} */}
+      {todos.map (function(todo){
+        return <Todo title={todo.title} desc={todo.desc} />
+      })}
     </>
   )
 }
