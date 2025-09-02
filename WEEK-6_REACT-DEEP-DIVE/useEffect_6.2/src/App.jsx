9ac41f1401dbc79b0b90c73650d914c017313fdd
Diotoara <aapka.5ryan@gmail.com>
2025-09-03 01:58:48 +0530

    todo_with_fetch

--- WEEK-6_REACT-DEEP-DIVE/useEffect_6.2/src/App.jsx
+++ WEEK-6_REACT-DEEP-DIVE/useEffect_6.2/src/App.jsx
@@ -1,13 +1,37 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
+  const [todos, setTodos] = useState([])
+
+  useEffect( () => {
+    fetch("http://localhost:3000/todos")
+      .then(async function(res){
+        const json = await res.json();
+        setTodos(json);
+      })
+    }, [] )
+    
+    
+    return (
+      <>
+
+      {
+        todos.length > 0 ? (<Todos title={todos[0].title} description={todos[0].content}/>) : <p>loading...</p>
+      }
+
+    </>
+  )
+}
+
+function Todos( {title, description}){
   return (
     <>
-      
+      <h1>{title}</h1>
+      <h5>{description}</h5>
     </>
   )
 }
