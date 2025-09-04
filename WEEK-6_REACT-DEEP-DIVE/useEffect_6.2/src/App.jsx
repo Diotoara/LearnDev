ef3ee52fd1948ee4e1ecd04e69e1eb5733ffcfaf
Diotoara <aapka.5ryan@gmail.com>
2025-09-05 03:01:58 +0530

    UseMemo

--- WEEK-6_REACT-DEEP-DIVE/useEffect_6.2/src/App.jsx
+++ WEEK-6_REACT-DEEP-DIVE/useEffect_6.2/src/App.jsx
@@ -1,45 +1,37 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
 
-  const [todos, setTodos] = useState([])
-  const [cnt, setCnt] = useState(0)
-
-  useEffect( () => {
-    fetch("http://localhost:3000/todos")
-      .then(async function(res){
-        const json = await res.json();
-        setTodos(json);
-      })
-    }, [cnt] )
-
-    function incnt(){
-      setCnt(cnt => cnt+1)
-    }    
-    
-    //make a button of update todo which update todos, instead of reloading to update.
+  // const [sum, SetSum] = useState(0);
+  const [input, SetInput] = useState(0)
 
 
-    return (
-      <>
-      <button onClick={incnt}>Click to update Todo</button>
-      {todos.map(todo => <Todos title={todo.title} description={todo.content} />)}
-
-    </>
-  )
-}
-
-function Todos( {title, description}){
+  let sum = useMemo( () => {
+    let fin = 0;
+    for(let i=1;i<=input;i++){
+      fin = fin + i;
+    }
+    return fin;
+  }, [input] )
+ 
   return (
     <>
-      <h1>{title}</h1>
-      <h5>{description}</h5>
-      <br />
+      <input onChange={(e) => 
+      SetInput(e.target.value)
+      } type="text" />
+      <Heasder num = {sum}/>
     </>
+
   )
+
+  function Heasder( {num} ){
+    return (
+      <h4>Sum is {num} </h4>
+    )
+  }
 }
 
 export default App
