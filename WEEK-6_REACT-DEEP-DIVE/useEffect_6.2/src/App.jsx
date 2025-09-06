9db579481bc297e6762137f02c74eca98f4acfaa
Diotoara <aapka.5ryan@gmail.com>
2025-09-06 22:07:23 +0530

    memo

--- WEEK-6_REACT-DEEP-DIVE/useEffect_6.2/src/App.jsx
+++ WEEK-6_REACT-DEEP-DIVE/useEffect_6.2/src/App.jsx
@@ -1,37 +1,26 @@
-import { useEffect, useMemo, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { memo, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
 
-  // const [sum, SetSum] = useState(0);
-  const [input, SetInput] = useState(0)
-
-
-  let sum = useMemo( () => {
-    let fin = 0;
-    for(let i=1;i<=input;i++){
-      fin = fin + i;
-    }
-    return fin;
-  }, [input] )
- 
-  return (
-    <>
-      <input onChange={(e) => 
-      SetInput(e.target.value)
-      } type="text" />
-      <Heasder num = {sum}/>
-    </>
-
-  )
-
-  function Heasder( {num} ){
-    return (
-      <h4>Sum is {num} </h4>
-    )
+  function onClick() {
+    console.log("child clicked")
   }
+
+  return <div>
+    <Child onClick={onClick} />
+    <button onClick={() => {
+      setCount(count + 1);
+    }}>Click me {count}</button>
+  </div>
 }
 
-export default App
+const Child = memo(({fnc}) => {
+  console.log("child render")
+
+  return <div>
+    <button onClick={fnc}>Button clicked</button>
+  </div>
+})
+
+export default App;
