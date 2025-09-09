df99f38128aff40e4116273976c7958864b7c5ee
Diotoara <aapka.5ryan@gmail.com>
2025-09-10 04:33:25 +0530

    useEffct

--- WEEK-6_REACT-DEEP-DIVE/week6.3/src/App.jsx
+++ WEEK-6_REACT-DEEP-DIVE/week6.3/src/App.jsx
@@ -4,32 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  console.log("ek aur re-render")
+
+  const [capita, setCapita] = useState({income:0})
+  const [side, setSide] = useState({money:0})
+
+  setTimeout( () => {
+    setCapita({'income' : 300});
+  },3000 );
+
+  setTimeout( () => {
+    setSide({'money' : 6000});
+  },6000 );
+
+  const total = (capita.income + side.money)
+
+
+  return(
+    <h3>you have a total of {total} money left in account.</h3>
   )
+
 }
+
+
+
+
 
 export default App
