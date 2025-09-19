fd15d2dc1ff356484cd0548f9fcc33ed38b7a8ad
Diotoara <aapka.5ryan@gmail.com>
2025-09-20 03:29:07 +0530

    signup-commit

--- WEEK8-TAILWIND/paytm/backend/routes/user.js
+++ WEEK8-TAILWIND/paytm/backend/routes/user.js
@@ -1,7 +1,21 @@
 const express = require("express")
-
 const userRouter = express.Router();
 
+const AllUser = require('../db')
 
+userRouter.get("/signup", async function(req,res){
+    const name = req.query.name
+    const email = req.query.email
+    const password = req.query.password
+    const Amount = 0
+    const newUser = new AllUser({
+        name,
+        email,
+        password,
+        Amount
+    })
+    await newUser.save();
+    res.send("New Account has been created")
+})
 
 module.exports = userRouter 
