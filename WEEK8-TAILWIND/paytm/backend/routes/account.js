78b22b1bc43c43d1aadb396c5a651b6fbde165d0
Diotoara <aapka.5ryan@gmail.com>
2025-09-24 05:56:25 +0530

    better-transcation-session

--- WEEK8-TAILWIND/paytm/backend/routes/account.js
+++ WEEK8-TAILWIND/paytm/backend/routes/account.js
@@ -15,34 +15,45 @@ router.get('/balance', authMiddleware , async(req,res)=>{
 
 
 router.post('/transffer', authMiddleware ,async(req,res)=>{
+
+    //session for each payment.
+    const session = await mongoose.startSession();
+    session.startTransaction();
+
+
     const to = req.body;   //enter user id
     const amount = req.body;
 
     const account = await AllAccounts.findOne({
         userId : req.userid,
-    });
+    }).session(session)
 
-    if(account.Balance < amount){
+    if(!account || account.Balance < amount){
         return res.status(400).json({
             message: "Insufficient balance"
         })
     }
     const receiver = await AllAccounts.findOne({
         userId:to,
-    })
+    }).session(session)
 
-    if(!receiver) return res.status(400).json({message:"Invalid Account"})
+    if(!receiver){
+        await session.abortTransaction();
+        return res.status(400).json({message:"Invalid Account"})
+    } 
     
     await AllAccounts.updateOne(
         {userId: req.userId},
         {$inc : {Balance:-amount}}
-    );
+    ).session(session);
 
     await AllAccounts.updateOne(
         {userId: to},
         {$inc : {Balance:amount}}
-    );
+    ).session(session);
 
+    //commiting transactions
+    await session.commitTransaction()
     res.json({
         message: "Transfer successful"
     })
