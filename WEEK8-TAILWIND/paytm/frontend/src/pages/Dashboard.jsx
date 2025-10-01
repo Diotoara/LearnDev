9e4a63ace9ad71728fe02e6b4f4d4a1626a41fc8
Diotoara <aapka.5ryan@gmail.com>
2025-10-01 17:46:03 +0530

    signin page

--- WEEK8-TAILWIND/paytm/frontend/src/pages/Dashboard.jsx
+++ WEEK8-TAILWIND/paytm/frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,14 @@
 
-function Dashboard(){
-    return(
-        <div>
-            Dashboard here
+
+const Dashboard = () => {
+  return (
+    <div className="bg-[#89B0AE] h-screen">
+        <div className="font-bold text-4xl">
+            Payments App
         </div>
-    )
+
+    </div>
+  )
 }
 
 export default Dashboard
--- WEEK8-TAILWIND/paytm/frontend/src/pages/Signin.jsx
+++ WEEK8-TAILWIND/paytm/frontend/src/pages/Signin.jsx
@@ -1,10 +1,36 @@
+import Heading from "../components/Heading"
+import SubHeading from "../components/SubHeading"
+import InputBox from "../components/InputBox"
+import Button from "../components/Button"
 
-function Signin(){
-    return(
-        <div>
-            Signin here
+const Signin = () => {
+  return (
+    <div className="bg-gray-400 h-screen w-screen flex justify-center items-center" >
+        <div className="bg-white h-90 w-80 rounded-2xl">
+            <div className="flex justify-center pt-5">
+                <Heading text={"Sign in"}  />
+            </div>
+            <div className="flex justify-center text-center pt-2 px-8 ">
+                <SubHeading text={"Enter you credentials to access your account"}  />
+            </div>
+            <div className="px-4">
+                <InputBox label={"Email"} placeholder={"johndoe@example.com"} />
+            </div>
+            <div className="px-4 pt-4">
+                <InputBox label={"Password"} placeholder={""} />
+            </div>
+            <div className="pt-4 flex justify-center ">
+                <Button text={"Sign in"} />
+            </div>
+            <div className="flex justify-center">
+                Dont have an account?
+                <div className="underline cursor-pointer">
+                    Sign Up
+                </div>
+            </div>
         </div>
-    )
+    </div>
+  )
 }
 
 export default Signin
