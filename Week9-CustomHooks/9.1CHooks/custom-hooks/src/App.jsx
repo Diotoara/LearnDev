4c4407fb95419f1587096ad7afa91e501b845d54
Diotoara <aapka.5ryan@gmail.com>
2025-10-06 23:31:08 +0530

    lifecycle event

--- Week9-CustomHooks/9.1CHooks/custom-hooks/src/App.jsx
+++ Week9-CustomHooks/9.1CHooks/custom-hooks/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [render, SetRender] = useState(true)
+  useEffect(()=>{
+    setInterval(()=>{
+      SetRender(p => !p)
+    },2000)
+    return () => clearInterval(interval)
+  },[])
+
 
   return (
     <>
-    <Mycompo/>
+    {render ? <MyComponent/> : <div></div>}
     </>
   )
 }
 
-function Mycompo() {
-  const [count, SetCount] = useState(0);
 
-  const increment = () => {
-    SetCount(count + 1);
-  }
 
-  return(
-    <>
-    <div>
-      {count}
-    </div>
-    <button onClick={increment} >Click Me {"<uWu>"} </button>
-    </>
-  )
+function MyComponent(){
+  useEffect(()=>{
+    console.log("mounted")
+
+    return() => {
+      console.log("unmounted bish")
+    }
+
+  },[])
+
+  return <div>
+    From Inside of Mycomponent
+  </div>
 
 }
 
