5142c85fef9963e1dfa0f5c73918eea67fdfc169
Diotoara <aapka.5ryan@gmail.com>
2025-10-08 01:30:04 +0530

    hooks-w-fetch

--- Week9-CustomHooks/9.1CHooks/custom-hooks/src/App.jsx
+++ Week9-CustomHooks/9.1CHooks/custom-hooks/src/App.jsx
@@ -2,25 +2,40 @@ import React, { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 
+function useTodos() {
 
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    const json =  axios.get('https://jsonplaceholder.typicode.com/posts/3')
+    .then(res => {
+      setTodos(res.data);
+      setLoading(false)
+    })
+  }, [])
+
+  return {todos, loading}
+}
 
 function App() {
-  const [render, SetRender] = useState(true)
-  useEffect(()=>{
-    setInterval(()=>{
-      SetRender(p => !p)
-    },2000)
-    return () => clearInterval(interval)
-  },[])
 
+  const {todos,loading} = useTodos()
 
+  if(loading){
+    return(
+      <h1>loading...</h1>
+    )
+  }
   return (
-    <>
-    {render ? <MyComponent/> : <div></div>}
-    </>
-  )
+    <div>
+      {todos.title}
+    </div>
+  );
+
 }
 
 
