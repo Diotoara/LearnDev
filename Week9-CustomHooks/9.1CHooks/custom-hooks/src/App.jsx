41e78afe29d80982193483e62905b37d73806c91
Diotoara <aapka.5ryan@gmail.com>
2025-10-08 01:43:15 +0530

    server-offline/online

--- Week9-CustomHooks/9.1CHooks/custom-hooks/src/App.jsx
+++ Week9-CustomHooks/9.1CHooks/custom-hooks/src/App.jsx
@@ -4,55 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import axios from 'axios'
 
+  function useIsOnline(){
+    const [mode, setMode] = useState(window.navigator.onLine)
+    useEffect(()=>{
+      window.addEventListener('online', () => setMode(true));
+      window.addEventListener('offline', () => setMode(false));
+    },[mode])
 
-function useTodos() {
+    return mode
 
-  const [todos, setTodos] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(()=>{
-    const json =  axios.get('https://jsonplaceholder.typicode.com/posts/3')
-    .then(res => {
-      setTodos(res.data);
-      setLoading(false)
-    })
-  }, [])
-
-  return {todos, loading}
-}
-
-function App() {
-
-  const {todos,loading} = useTodos()
-
-  if(loading){
-    return(
-      <h1>loading...</h1>
-    )
   }
-  return (
-    <div>
-      {todos.title}
-    </div>
-  );
 
-}
+  function App() {
 
+    const mode = useIsOnline()
 
-
-function MyComponent(){
-  useEffect(()=>{
-    console.log("mounted")
-
-    return() => {
-      console.log("unmounted bish")
-    }
-
-  },[])
-
-  return <div>
-    From Inside of Mycomponent
-  </div>
+    return(
+      <div>
+        {console.log(window.navigator.onLine)}
+        {console.log(mode)}
+        {mode ? <h1>Server is Online</h1> : <h2>Server is offline</h2>}
+      </div>
+    )
 
 }
 
