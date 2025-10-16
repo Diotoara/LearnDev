b3089da0a80eae033162075e50e7cff2853d30ef
Diotoara <aapka.5ryan@gmail.com>
2025-10-16 22:15:36 +0530

    changes

--- WEEK8-TAILWIND/8.1-tailwind/src/components/Sidebar.jsx
+++ WEEK8-TAILWIND/8.1-tailwind/src/components/Sidebar.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div className=' bg-blue-200 w-[224px] min-h-screen'>
+    <div className=' h-full bg-blue-200 w-[224px] min-h-screen'>
+      <ul>
+        <li>heloo</li>
+        <li>heloo</li>
+      </ul>
     </div>
   )
 }
