c6ec25ae203d866a5e9b3ad6b925e67046f0b24c
Diotoara <aapka.5ryan@gmail.com>
2025-10-20 00:19:07 +0530

    client connect

---
+++ WEEK10-POSTGRE/2-PRISMA/prisma/migrations/20251019181900_todo_users/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Todo" (
+    "id" SERIAL NOT NULL,
+    "Title" TEXT NOT NULL,
+    "Done" BOOLEAN NOT NULL DEFAULT false,
+    "Description" TEXT,
+    "UserId" INTEGER NOT NULL,
+
+    CONSTRAINT "Todo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Todo" ADD CONSTRAINT "Todo_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
