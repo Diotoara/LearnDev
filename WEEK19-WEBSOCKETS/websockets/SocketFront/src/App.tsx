6681f8f50627861c1e711c13f047a4a39471be23
Diotoara <aapka.5ryan@gmail.com>
2026-01-01 21:45:50 +0530

    updates

--- WEEK19-WEBSOCKETS/websockets/SocketFront/src/App.tsx
+++ WEEK19-WEBSOCKETS/websockets/SocketFront/src/App.tsx
@@ -4,6 +4,7 @@ import './App.css'
 function App() {
   const [socket, setSocket] = useState<WebSocket | null>(null)
   const [messages, setMessages] = useState<any>([])
+  const [sendText, setSendText] = useState<string>("")
 
   useEffect(()=>{
     const socket = new WebSocket("ws://localhost:8080");
@@ -14,6 +15,10 @@ function App() {
     socket.onmessage = (message) => {
       console.log("received message : ", message);
       setMessages((m:any)=>[...m, message.data])
+    }
+
+    return ()=> {
+      socket.close()
     }
   },[])
 
@@ -27,11 +32,20 @@ function App() {
 
   return (
     <div>
+      <div>
+        <input type="text" onChange={(e)=>setSendText(e.target.value)} />
+        <button onClick={()=>{
+          socket.send(sendText)
+        }} >Send</button>
+      </div>
+
+      <div>
       {messages.map((msg:any, index:number) => (
         <div key={index}>
           {msg}
         </div>
       ))}
+      </div>
     </div>
   )
 }
