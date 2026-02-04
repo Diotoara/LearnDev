9c759adf21280440ae14977254892e2e0638dd42
diotoara <aapka.5ryan@gmail.com>
2026-02-05 01:04:32 +0530

    user join n chat

--- WEEK19-1-WEBSOCKETS/chat-app/src/index.ts
+++ WEEK19-1-WEBSOCKETS/chat-app/src/index.ts
@@ -1,22 +1,54 @@
-import {WebSocketServer} from "ws";
+import {WebSocketServer , WebSocket } from "ws";
 
 const wss = new WebSocketServer({port:8080})
 console.log("server running on ws://localhost:8080")
 
-let user:number = 0;
-let allSockets = [];
+interface User {
+    socket : WebSocket,
+    room : string,
+}
 
-wss.on("connection",(socket)=>{
-    allSockets.push(socket);
-    socket.send("you are connected to server")
-    console.log("user #",++user);
+interface JoinM {
+    type : "join"
+    payload : {
+        roomId : string,
+    }
+}
+
+interface ChatM {
+    type : "chat"
+    payload : {
+        message : string,
+    }
+}
+
+type Mess = JoinM | ChatM
+
+let allSockets : User[] = [];
+
+wss.on("connection", (socket)=>{
 
     socket.on("message",(message)=>{
-        for(let i=0;i<allSockets.length;i++){
-            const s = allSockets[i];
-            s?.send(message.toString() + " :from the server")
+        const parsedMessage:Mess = JSON.parse(message as unknown as string);
+        
+        if(parsedMessage.type == "join"){
+            allSockets.push({
+                socket : socket,
+                room : parsedMessage.payload.roomId
+            })
+            console.log(allSockets)
         }
-       
+
+        if(parsedMessage.type == "chat"){
+            //find the user with this socket, and then its room.
+            const currentUserRoom = allSockets.find((x)=>x.socket==socket)?.room
+
+            //now select sockets with that roomId && send message to them
+            allSockets
+                .filter(s => s.room ==currentUserRoom)
+                .forEach(s => s.socket.send(parsedMessage.payload.message))
+        }
+
     })
 
 })
